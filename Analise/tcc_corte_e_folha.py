c02175714769deb09e7eed25ec11ebba057d30d2
caio.ribeiro@scania.com <caio.ribeiro@scania.com>
2021-10-17 13:23:03 -0300

    organizando programa final

--- Analise/tcc_corte_e_folha.py
+++ Analise/tcc_corte_e_folha.py
@@ -13,7 +13,7 @@ from skimage.filters import threshold_otsu
 from skimage.segmentation import clear_border
 
 ########################################## VARIÁVEIS DO ENSAIO #####################################
-path = 'H:\Caio Quaglierini\Documents\TCC\'
+path_raiz = 'H:\Caio Quaglierini\Documents\TCC\'
 bioassay = 9895
 lagarta_lista = []
 folha_lista = []
@@ -29,19 +29,19 @@ lagarta = 'Chrysodeixis includens'
 group_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 #Percorre os paths de fotos dos grupos
 for group in group_list:
-  group_path = path+str(bioassay)+'/Fotos_Originais/'+str(bioassay)+'_Grupo_'+group
+  group_path = path_raiz+str(bioassay)+'/Fotos_Originais/'+str(bioassay)+'_Grupo_'+group
   ImgFile = group_path
   # IMAGEM
-  src1 = cv.imread(ImgFile,1) #Colorida
-  src0 = cv.imread(ImgFile,0) #grayscale
+  img_rgb = cv.imread(ImgFile,1) #Colorida
+  img_gray = cv.imread(ImgFile,0) #grayscale
 
 ########################################## IDENTIFICAR CÍRCULOS ######################################
 # QUADRANTE
-src1 = src1[200:900, 570:1320].copy()
-src0 = src0[200:900, 570:1320].copy()
+img_rgb = img_rgb[200:900, 570:1320].copy()
+img_gray = img_gray[200:900, 570:1320].copy()
 
 # CONTORNOS
-rgb_planes = cv.split(src0)
+rgb_planes = cv.split(img_gray)
 result_planes = []
 result_norm_planes = []
 for plane in rgb_planes:
@@ -81,31 +81,23 @@ df = df.sort_values(by=['group_index', 'y'])
 df['cell_index'] = range(1, 17)
 
 # DESENHAR CÍRCULOS NA IMAGEM
-circles_image = src1.copy()
+circles_image = img_rgb.copy()
 for index, row in df.iterrows():
     # desenhar a borda do círculo
     cv.circle(circles_image,(df.x[index],df.y[index]),
               df.radius[index],(0,255,0),2)
     # desenhar o centro do círculo
     cv.circle(circles_image,(df.x[index],df.y[index]),2,(0,0,255),3)
-#cv2_imshow(circles_image)
 
 ########################################## RECORTAR CÍRCULOS ######################################
 i = 0
-fig, axes = plt.subplots(4, 4, figsize=(16, 8), sharex=True, sharey=True)
-ax = axes.ravel()
 for index, row in df.iterrows():
   # crop image
-  image = src1[(df.y[index]-40):(df.y[index]+40),(df.x[index]-40):(df.x[index]+40)]
+  image = img_rgb[(df.y[index]-40):(df.y[index]+40),(df.x[index]-40):(df.x[index]+40)]
   # NOME E PATH DAS 16 IMAGENS
   image_name = str(df.group_index[index])+str(df.cell_index[index])+'.jpg'
-  cv.imwrite('/content/drive/MyDrive/Mauá/TCC/Códigos/Imagens/Imagens cortadas/'+image_name, image) 
-  #ax[i].imshow(image)
-  #ax[i].set_title(image_name)
+  cv.imwrite(path_raiz + str(bioassay) + '/Fotos_Cortadas/' + str(bioassay) + '_Foto_' + image_name, image) 
   i+=1
-#for a in ax.ravel():
-#    a.axis('off')
-#fig.tight_layout()
 
 ################################################# MAIN #############################################
 def main(cell):
@@ -113,7 +105,7 @@ def main(cell):
   for group in group_list:
     #Percorre os paths de fotos das folhas cortadas
     for i in range(1,17):
-      img_path = path+str(bioassay)+'/Fotos_Cortadas/'+str(bioassay)+'_Foto_'+group+str(i)
+      img_path = path_raiz+str(bioassay)+'/Fotos_Cortadas/'+str(bioassay)+'_Foto_'+group+str(i)
       arquivo = img_path + ".jpg"
       image = cv.imread(arquivo,1)
       image_gray = cv.imread(arquivo,0)
@@ -147,62 +139,62 @@ for i in range(len(lista)):
   n += 0.05
 
 def csv_inicial():
-  df = pd.DataFrame()
-  df = pd.DataFrame(folha_lista)
-  df = df.rename(columns={'area':'area_folha'})
-  df = df.apply(pd.Series.explode)
+  df_inicial = pd.DataFrame()
+  df_inicial = pd.DataFrame(folha_lista)
+  df_inicial = df_inicial.rename(columns={'area':'area_folha'})
+  df_inicial = df_inicial.apply(pd.Series.explode)
   # Filtrar áreas
-  # df = df[(df.area_folha > 700) & (df.area_folha < 3000)] # grãos
-  df = df.groupby("Célula_ID").sum().sort_values(by=['Célula_ID'])
-  df = df.reset_index()
-  # df.drop(columns='label', inplace=True)
+  # df_inicial = df_inicial[(df_inicial.area_folha > 700) & (df_inicial.area_folha < 3000)] # grãos
+  df_inicial = df_inicial.groupby("Célula_ID").sum().sort_values(by=['Célula_ID'])
+  df_inicial = df_inicial.reset_index()
+  # df_inicial.drop(columns='label', inplace=True)
 
-  df2 = pd.DataFrame()
-  df2 = pd.DataFrame(lagarta_lista)
-  df2 = df2.apply(pd.Series.explode)
-  df2 = df2.groupby("Célula_ID").sum().sort_values(by=['Célula_ID'])
-  df2 = df2.reset_index()
+  df_lagarta = pd.DataFrame()
+  df_lagarta = pd.DataFrame(lagarta_lista)
+  df_lagarta = df_lagarta.apply(pd.Series.explode)
+  df_lagarta = df_lagarta.groupby("Célula_ID").sum().sort_values(by=['Célula_ID'])
+  df_lagarta = df_lagarta.reset_index()
 
   # Converter em escala micron
-  df["A0_Folha"] = df['area_folha'] * (scale**2)
-  df['A0_Lagarta'] = df2['area'] * (scale**2)
+  df_inicial["A0_Folha"] = df_inicial['area_folha'] * (scale**2)
+  df_inicial['A0_Lagarta'] = df_lagarta['area'] * (scale**2)
 
-  df['Data início'] = horario_atual
-  df['Data final'] = ''
-  df['Grupo_ID'] = 'A'
-  df['Condição'] = condicao
-  df['Planta'] = planta
-  df['Lagarta'] = lagarta
-  df['Técnico'] = operador
-  df["A'_Folha"] = ''
-  df["A'_Lagarta"] = ''
-  df['Redução (%)'] = ''
-  df['Aumento (%)'] = ''
-  df['Status da lagarta'] = ''
-  df = df[['Data início', 'Data final', 'Técnico', 'Planta', 'Lagarta', 'Condição', 'Grupo_ID', 'Célula_ID',
+  df_inicial['Data início'] = horario_atual
+  df_inicial['Data final'] = ''
+  df_inicial['Grupo_ID'] = 'A'
+  df_inicial['Condição'] = condicao
+  df_inicial['Planta'] = planta
+  df_inicial['Lagarta'] = lagarta
+  df_inicial['Técnico'] = operador
+  df_inicial["A'_Folha"] = ''
+  df_inicial["A'_Lagarta"] = ''
+  df_inicial['Redução (%)'] = ''
+  df_inicial['Aumento (%)'] = ''
+  df_inicial['Status da lagarta'] = ''
+  df_inicial = df_inicial[['Data início', 'Data final', 'Técnico', 'Planta', 'Lagarta', 'Condição', 'Grupo_ID', 'Célula_ID',
           'A0_Folha', "A'_Lagarta", 'Redução (%)', 'A0_Lagarta', "A'_Lagarta", 'Aumento (%)', 'Status da lagarta']]
-  df.to_csv(path + str(bioassay) +str(bioassay) + '.csv')
-  return df
+  df_inicial.to_csv(path_raiz + str(bioassay) +str(bioassay) + '.csv')
+  return df_inicial
 
 def csv_final():
-  db_final = pd.read_csv(path + str(bioassay) +str(bioassay) + '.csv')
-  db_final['Área final da planta'] = teste # teste depois apagar
-  db_final['Redução (%)'] = round((db_final['Área inicial da planta'] - db_final['Área final da planta']) / db_final['Área inicial da planta'] *100, 2)
-  db_final['Data final'] = hora_atual
+  df_final = pd.read_csv(path_raiz + str(bioassay) +str(bioassay) + '.csv')
+  df_final['Área final da planta'] = teste # teste depois apagar
+  df_final['Redução (%)'] = round((df_final['Área inicial da planta'] - df_final['Área final da planta']) / df_final['Área inicial da planta'] *100, 2)
+  df_final['Data final'] = hora_atual
   # lista de condições para nível de redução
   conditions1 = [
-      (db_final['Redução (%)'] >= 90), (db_final['Redução (%)'] < 90) & (db_final['Redução (%)'] >= 80),
-       (db_final['Redução (%)'] < 80) & (db_final['Redução (%)'] >= 70), (db_final['Redução (%)'] < 70) & (db_final['Redução (%)'] >= 60), 
-       (db_final['Redução (%)'] < 60) & (db_final['Redução (%)'] >= 50), (db_final['Redução (%)'] < 50) & (db_final['Redução (%)'] >= 40), 
-       (db_final['Redução (%)'] < 40) & (db_final['Redução (%)'] >= 30), (db_final['Redução (%)'] < 30) & (db_final['Redução (%)'] >= 20), 
-       (db_final['Redução (%)'] < 20) & (db_final['Redução (%)'] >= 10), (db_final['Redução (%)'] < 10)
+      (df_final['Redução (%)'] >= 90), (df_final['Redução (%)'] < 90) & (df_final['Redução (%)'] >= 80),
+       (df_final['Redução (%)'] < 80) & (df_final['Redução (%)'] >= 70), (df_final['Redução (%)'] < 70) & (df_final['Redução (%)'] >= 60), 
+       (df_final['Redução (%)'] < 60) & (df_final['Redução (%)'] >= 50), (df_final['Redução (%)'] < 50) & (df_final['Redução (%)'] >= 40), 
+       (df_final['Redução (%)'] < 40) & (df_final['Redução (%)'] >= 30), (df_final['Redução (%)'] < 30) & (df_final['Redução (%)'] >= 20), 
+       (df_final['Redução (%)'] < 20) & (df_final['Redução (%)'] >= 10), (df_final['Redução (%)'] < 10)
       ]
   # # lista de valores para cada condição
   values1 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-  db_final['Nível de redução'] = np.select(conditions1, values1)
-  db_final.drop(db_final.columns[0], axis=1, inplace=True)
-  db_final.to_csv(path + str(bioassay) +str(bioassay) + '.csv')
-  return db_final
+  df_final['Nível de redução'] = np.select(conditions1, values1)
+  df_final.drop(df_final.columns[0], axis=1, inplace=True)
+  df_final.to_csv(path_raiz + str(bioassay) +str(bioassay) + '.csv')
+  return df_final
 
 ############################################### REALIZAR TESTE #############################################
 def Cortar_Imagens(group_path):
@@ -213,7 +205,7 @@ def Analisar_Imagens(img_path):
 
 #Execução da Função de Corte das Imagens
 for group in group_list:
-  group_path = path+str(bioassay)+'/Fotos_Originais/'+str(bioassay)+'_Grupo_'+group
+  group_path = path_raiz+str(bioassay)+'/Fotos_Originais/'+str(bioassay)+'_Grupo_'+group
   Cortar_Imagens(group_path)
 
 #Execução da Função de Análise das Imagens
@@ -222,7 +214,7 @@ ListCell = []
 ListArea = []
 for group in group_list:
   for i in range(1,17):
-    img_path = path+str(bioassay)+'/Fotos_Cortadas/'+str(bioassay)+'_Foto_'+group+str(i)
+    img_path = path_raiz+str(bioassay)+'/Fotos_Cortadas/'+str(bioassay)+'_Foto_'+group+str(i)
     img_area = Analisar_Imagens(img_path)
     ListArea.append(img_area)
     ListCell.append(i)
