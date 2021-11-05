84ec3fd88ca9e0cdbd1855ee7b2a3d3b1b775748
Henrique Vicente <henriquevicente25@hotmail.com>
2021-11-04 23:06:11 -0300

    Json change screen rasp

--- Raspberry - Interface/tcc_app_raspberry.py
+++ Raspberry - Interface/tcc_app_raspberry.py
@@ -27,7 +27,9 @@ class HomeWindow(Screen):
     def __init__(self, **kwargs):
         super(HomeWindow, self).__init__(**kwargs)
         #Cria um relógio que chamará a função 'VerificaJson' a cada 10 segundos
-        Clock.schedule_interval(self.VerificaJson, 10)
+        Clock.schedule_interval(self.VerificaJson, 3)
+
+    screen = "Home"
 
     #Função para atualizar a data    
     def VerificaJson(self, dt):
@@ -41,9 +43,12 @@ class HomeWindow(Screen):
             config_file.close()    
         #Verifica o status
         tela = JsonToPi['status']
-        #Seleciona a tela 
-        if (tela == 'running'):
+        #Aletera a tela
+        if (tela == 'running' and self.screen != "Ensaio"):
             self.parent.current = 'Ensaio'
+            self.screen = "Ensaio"
+        elif (tela != 'running' and self.screen == "Ensaio"):
+            self.screen = "Home"
 
     #Função para abrir aplicativo de configurações de rede    
     def Abrir_Configuracoes(self):
@@ -54,7 +59,85 @@ class HomeWindow(Screen):
 
 
 class EnsaioWindow(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(EnsaioWindow, self).__init__(**kwargs)
+        #Cria um relógio que chamará a função 'Atualiza' a cada 10 segundos
+        Clock.schedule_interval(self.Atualiza, 5)
+
+
+    #Função para atualizar as informações    
+    def Atualiza(self, dt):
+        #Carrega O arquivo Json
+        path = os.getcwd()+'\\Raspberry - Interface\\Connection\\to_pi.json'
+        with open(path, 'r', encoding="utf-8") as config_file:
+            data=config_file.read()
+            JsonToPi = json.loads(data) 
+            config_file.close()    
+        #Verifica o status
+        tela = JsonToPi['status']
+        #Aletera o widget
+        if (tela == 'sleeping'):
+            try:
+                #Exibe widget botão finalizar
+                self.add_widget(self.ids.FinalizaEnsaio)
+            except:
+                pass
+    
+    def RealizaEnsaio(self):
+        #Verificar se a porta está aberta:
+        if (False):
+            #PopUp avisando que a porta está aberta
+            box = BoxLayout(orientation='vertical',padding=10,spacing=10)
+            popup = Popup(title='Atenção!', content=box, auto_dismiss=False, size_hint=(0.4,0.3))
+            texto = Label(text='A porta está aberta')
+            botao = Button(text='ok',on_release=popup.dismiss)
+            box.add_widget(texto)
+            box.add_widget(botao)
+            popup.open()
+        else:
+            #Remove widget botão iniciar
+            self.remove_widget(self.ids.IniciaEnsaio)
+            #Exibe widget botão finalizar
+            self.remove_widget(self.ids.FinalizaEnsaio)
+            #Retorna a barra para P1
+            self.ids.Logs.text = "Levando cameras para P1" 
+            #Acende leds inferiores
+            self.ids.Logs.text = "Ligando Leds inferiores"
+            #Acende leds superiores
+            self.ids.Logs.text = "Ligando Leds superiores"
+            #Captura as 4 primeiras fotos
+            self.ids.Logs.text = "Tirando fotos A-B-C-D"
+            #Avança a barra para P2
+            self.ids.Logs.text = "Levando cameras para P2"
+            #Captura as 4 últimas fotos
+            self.ids.Logs.text = "Tirando fotos E-F-G-H"
+            #Apaga leds inferiores
+            self.ids.Logs.text = "Desligando Leds inferiores"
+            #Apaga leds superiores
+            self.ids.Logs.text = "Desligando Leds superiores"
+            
+            
+    def FinalizaEnsaio(self):
+        #Exibe widget botão iniciar
+        self.add_widget(self.ids.IniciaEnsaio)
+        #Remove widget botão finalizar
+        self.remove_widget(self.ids.FinalizaEnsaio)
+        #Aletera a tela
+        self.parent.current = 'Home'
+
+    '''PC - manda por a bandeja e dar ok 
+    ok do pc altera json
+    ao entrar na nova tela o rasp pergunta se o ensaio esta certo, bandeja posicionada e verifica porta
+    roda a rotina de teste
+    altera json e aguarda na tela de finalizado
+    pc verifica json e coleta as fotos
+    pc altera json do rasp que volta pra tela home '''
+    
+
+
+
+
+
 
 
 
