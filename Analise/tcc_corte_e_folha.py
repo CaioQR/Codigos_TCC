7e2dac36d2ad8a718924ab64a0e1652d11f8806c
CaioQR <82450879+CaioQR@users.noreply.github.com>
2021-11-23 20:30:37 -0300

    Add files via upload

--- Analise/tcc_corte_e_folha.py
+++ Analise/tcc_corte_e_folha.py
@@ -25,6 +25,17 @@ operador = 'João'
 planta = 'Soja'
 lagarta = 'Chrysodeixis includens'
 
+######################################### MAHALANOBIS #####################################
+import scipy as sp
+def mahalanobis(x=None, data=None, cov=None):
+    x_minus_mu = x - np.mean(data)
+    if not cov:
+        cov = np.cov(data.values.T)
+    inv_covmat = sp.linalg.inv(cov)
+    left_term = np.dot(x_minus_mu, inv_covmat)
+    mahal = np.dot(left_term, x_minus_mu.T)
+    return mahal.diagonal()
+
 ################################################ IMAGEM ##############################################
 def Cortar_Imagens(group_path):
 #Percorre os paths de fotos dos grupos
@@ -36,11 +47,12 @@ def Cortar_Imagens(group_path):
     img_gray = cv.imread(ImgFile,0) #grayscale
 ########################################## IDENTIFICAR CÍRCULOS ######################################
     # QUADRANTE
-    img_rgb = img_rgb[200:900, 570:1320].copy()
-    img_gray = img_gray[200:900, 570:1320].copy()
+    src1 = src1[int(src1.shape[0]/2-400):int(src1.shape[0]/2+400), int(src1.shape[1]/2-400):int(src1.shape[1]/2+400)].copy()
+    #[200:900, 570:1320]
+    src0 = src0[int(src0.shape[0]/2-400):int(src0.shape[0]/2+400), int(src0.shape[1]/2-400):int(src0.shape[1]/2+400)].copy()
 
     # CONTORNOS
-    rgb_planes = cv.split(img_gray)
+    rgb_planes = cv.split(src0)
     result_planes = []
     result_norm_planes = []
     for plane in rgb_planes:
@@ -55,41 +67,54 @@ def Cortar_Imagens(group_path):
     maxValue = 255
     retval, dst = cv.threshold(result, 0, maxValue, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
 
-    # CÍRCULOS
+    # Achar os círculos
     circles = cv.HoughCircles(dst,
                               cv.HOUGH_GRADIENT,
-                              dp=1.2,
-                              minDist=100,
-                              param1=100,
-                              param2=8,
-                              minRadius=40,
-                              maxRadius=45)
+                              dp=1.15,
+                              minDist=110,
+                              param1=110,
+                              param2=9,
+                              minRadius=55,
+                              maxRadius=58)
+    # 1.15, 110, 100, 8, 46, 50
+    # Mudar para inteiro
     circles = np.uint16(np.around(circles))
 
     # LISTA DE COORDENADAS DOS CÍRCULOS
     center = []
     for i in circles[0,:]:
         center.append([i[0],i[1], i[2]])
+    # treat circles with pandas dataframe 
     df = pd.DataFrame(center, columns=['x','y','radius'])
-    df = df.sort_values(by='x').reset_index(drop=True).head(n=16) 
+    # order circles by x position and limit result to 16 circles (this will exclude the other circles)
+    df = df.sort_values(by='x').reset_index(drop=True).head(n=30) 
+    # creates group_index
     df['group_index'] = str(group)
+    # creates cell_index
     df = df.sort_values(by=['group_index', 'y'])
-    df['cell_index'] = range(1, 17)
+    df['cell_index'] = range(1, len(df.index)+1)
+    
+    # Mahalanobis
+    df_mahala = df.copy()
+    df_mahala['mahala'] = mahalanobis(df_mahala[['x','y']], df_mahala[['x','y']])
 
     # DESENHAR CÍRCULOS NA IMAGEM
-    circles_image = img_rgb.copy()
+    # desenhar os círculos na imagem
+    circles_image = src1.copy()
+    df = df_mahala.drop(df_mahala[df_mahala.mahala < 0.001].index)
+    df = df.drop(df[df.mahala > 4].index)
     for index, row in df.iterrows():
-      # desenhar a borda do círculo
-      cv.circle(circles_image,(df.x[index],df.y[index]),
-                df.radius[index],(0,255,0),2)
-      # desenhar o centro do círculo
-      cv.circle(circles_image,(df.x[index],df.y[index]),2,(0,0,255),3)
+        # desenhar a borda do círculo
+        cv.circle(circles_image,(df.x[index],df.y[index]),
+                  df.radius[index],(0,255,0),2)
+        # desenhar o centro do círculo
+        cv.circle(circles_image,(df.x[index],df.y[index]),2,(0,0,255),3)
 
 ########################################## RECORTAR CÍRCULOS ######################################
     i = 0
     for index, row in df.iterrows():
       # crop image
-      image = img_rgb[(df.y[index]-40):(df.y[index]+40),(df.x[index]-40):(df.x[index]+40)]
+      image = src1[(df.y[index]-70):(df.y[index]+70),(df.x[index]-70):(df.x[index]+70)]
       # NOME E PATH DAS 16 IMAGENS
       image_name = str(df.group_index[index])+str(df.cell_index[index])+'.jpg'
       cv.imwrite(path_raiz + str(bioassay) + '/Fotos_Cortadas/' + str(bioassay) + '_Foto_' + image_name, image) 
